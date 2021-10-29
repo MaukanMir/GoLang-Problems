b3d9e79638b03ea8474a7131adbe2cffdb1158ca
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-10-29 13:47:31 -0400

    Create containsDuplicate.go

---
+++ containsDuplicate.go
@@ -0,0 +1,27 @@
+<-- Directions -->
+217. Contains Duplicate
+Easy
+
+2772
+
+890
+
+Add to List
+
+Share
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+<-- End Of Directions -->
+
+func containsDuplicate(nums []int) bool {
+    visited := map[int]bool{}
+    
+    for i:= range nums{
+        check:= nums[i]
+        if visited[check] != true{
+            visited[check] = true
+        }else{
+            return true
+        }
+    }
+    return false
+}
