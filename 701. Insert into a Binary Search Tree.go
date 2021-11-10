4f1368e6cc72800f5cad2eeeb98585713316a3b3
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-11-10 08:33:33 -0500

    Update 701. Insert into a Binary Search Tree.go

--- 701. Insert into a Binary Search Tree.go
+++ 701. Insert into a Binary Search Tree.go
@@ -53,4 +53,46 @@ func insertIntoBST(root *TreeNode, val int) *TreeNode {
 }
 
 
+Success
+Details 
+Runtime: 28 ms, faster than 90.05% of Go online submissions for Insert into a Binary Search Tree.
+Memory Usage: 7.5 MB, less than 39.27% of Go online submissions for Insert into a Binary Search Tree.
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+    
+    node := root
+    
+    for node != nil{
+        if node.Val < val{
+            
+            if node.Right == nil{
+                node.Right = &TreeNode{Val: val}
+                return root
+            }else{
+                node = node.Right
+            }
+        }else{
+            if node.Left == nil{
+                node.Left = &TreeNode{Val: val}
+                return root
+            }else{
+                node = node.Left
+            }
+        }
+    }
+    
+    return &TreeNode{Val: val}
+    
+}
+
+
 
