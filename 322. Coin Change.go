523e925683eb3aca14067787d147fd0e486f83d5
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-06 16:27:30 -0500

    Create 322. Coin Change.go

---
+++ 322. Coin Change.go
@@ -0,0 +1,64 @@
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+// Example 1:
+
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+// Example 2:
+
+// Input: coins = [2], amount = 3
+// Output: -1
+// Example 3:
+
+// Input: coins = [1], amount = 0
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= coins.length <= 12
+// 1 <= coins[i] <= 231 - 1
+// 0 <= amount <= 104
+
+// Success
+// Details 
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Coin Change.
+// Memory Usage: 6.5 MB, less than 98.21% of Go online submissions for Coin Change.
+
+
+
+func coinChange(coins []int, amount int) int {
+    numOfCoins := make([]int, amount+1)
+    for i:= range numOfCoins{
+        numOfCoins[i] = math.MaxInt32
+    }
+    numOfCoins[0] =0
+    
+    for _, coin := range coins{
+        
+        for total := range numOfCoins{
+            if coin <= total{
+                numOfCoins[total] = min(numOfCoins[total], numOfCoins[total- coin]+1)
+            }
+        }
+    }
+    
+    if numOfCoins[amount] != math.MaxInt32{
+        return numOfCoins[amount]
+    }
+    return -1
+}
+
+func min(a,b int)int{
+    if a< b{
+        return a
+    }
+    return b
+}
